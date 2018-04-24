3cf9f216e5ca6d68abe316ff62a8a09be5522584
Chau_laptop <longchau21@gmail.com>
2018-04-24 09:37:21 +0200

    Chapter 5: User registration. Hot fixes 2.

--- app/routes.py
+++ app/routes.py
@@ -40,7 +40,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-    return redirect(next_page)
+        return redirect(next_page)
 
 
 @app.route('/logout')
